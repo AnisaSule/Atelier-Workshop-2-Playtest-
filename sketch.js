9ff547e4a8b2a0309007c9b0a53ebeeb4fd28bed
AnisaSule <sulea@ocadu.ca>
2025-10-07 18:37:30 -0400

    center gif

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+let horse; 
+function preload() {
+	horse = loadImage('/assets/horse.gif')
+}
+
+function setup() 
+{
+	createCanvas (windowWidth,windowHeight);
+	
+}
+
+function draw()
+{
+
+background(255);
+translate(50, 50);
+image(horse, 0, 0,width, height 
+
+)
+
+  
+}
