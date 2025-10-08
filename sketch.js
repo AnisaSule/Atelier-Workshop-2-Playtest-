ee047192f56eeef5fe549f0bd4bbd2199a6b4bf1
AnisaSule <sulea@ocadu.ca>
2025-10-08 13:56:23 -0400

    button

--- sketch.js
+++ sketch.js
@@ -1,28 +1,36 @@
+let permissionButton;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(220);
-	textAlign(CENTER, CENTER);
-	textSize(24);
-	text("Tap to enable motion sensors", width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  text("Tap the button to enable sensors", width/2, height/2 - 40);
+
+  permissionButton = createButton("Enable Motion Sensors");
+  permissionButton.position(width/2 - 100, height/2);
+  permissionButton.size(200, 40);
+  permissionButton.mousePressed(requestSensorPermission);
+}
+
+function requestSensorPermission() {
+  if (typeof DeviceOrientationEvent !== 'undefined' &&
+      typeof DeviceOrientationEvent.requestPermission === 'function') {
+    DeviceOrientationEvent.requestPermission()
+      .then(response => {
+        if (response === 'granted') {
+          permissionButton.hide();
+          text("✓ Permission granted!", width/2, height/2 + 60);
+          window.sensorsEnabled = true;
+        } else {
+          text("✗ Permission denied.", width/2, height/2 + 60);
+        }
+      })
+      .catch(err => {
+        text("Error: " + err.message, width/2, height/2 + 60);
+      });
+  } else {
+    text("Permission not required on this device.", width/2, height/2 + 60);
+    window.sensorsEnabled = true;
   }
-  
-  function touchStarted() {
-	if (typeof DeviceOrientationEvent !== 'undefined' &&
-		typeof DeviceOrientationEvent.requestPermission === 'function') {
-	  DeviceOrientationEvent.requestPermission()
-		.then(response => {
-		  if (response === 'granted') {
-			text("Permission granted!", width/2, height/2 + 40);
-		  } else {
-			text("Permission denied.", width/2, height/2 + 40);
-		  }
-		})
-		.catch(err => {
-		  text("Error: " + err.message, width/2, height/2 + 40);
-		});
-	} else {
-	  text("Permission not required on this device.", width/2, height/2 + 40);
-	}
-	return false;
-  }
-  
+}
